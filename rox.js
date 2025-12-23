36c0870e6519f0e6f5ee66fed724a67056654ee9
rafavd <rafarovd@gmail.com>
2025-12-23 01:21:23 -0300

    Update rox.js

--- rox.js
+++ rox.js
@@ -1,5 +1,6 @@
 const { connect } = require("puppeteer-real-browser");
 const fs = require("fs");
+const loginRox = require("./loginRox");
 
 require("dotenv").config({ quiet: true });
 
@@ -10,15 +11,8 @@ const LOCALSTORAGE_PATH = "localstorage.json";
 const rox = async () => {
   const { page, browser } = await connect({
     args: ["--start-maximized"],
-    turnstile: true,
+    turnstile: false,
     headless: false,
-    // disableXvfb: true,
-    // proxy: {
-    //   host: "",
-    //   port: ,
-    //   username: "",
-    //   password: "",
-    // },
     customConfig: {},
     connectOption: {
       defaultViewport: null,
@@ -59,6 +53,24 @@ const rox = async () => {
 
     await new Promise((r) => setTimeout(r, 2000));
 
+    const saldo = await page.evaluate(() => {
+      const el = document.querySelector("span.css-1jxf684.r-jwli3a.r-n644vd");
+      return el ? el.textContent.trim() : null;
+    });
+
+    const auth = await page.evaluate(() => {
+      return document.body.innerHTML.includes("Algo deu errado");
+    });
+
+    if (!saldo || auth) {
+      console.log("Erro de autenticação. Verifique o login.");
+      fs.existsSync(COOKIES_PATH) && fs.unlinkSync(COOKIES_PATH);
+      fs.existsSync(LOCALSTORAGE_PATH) && fs.unlinkSync(LOCALSTORAGE_PATH);
+      await browser.close();
+      await loginRox();
+      return await rox();
+    }
+
     for (let i = 0; i < 5; i++) {
       const clica = await page.evaluate(() => {
         const botao = Array.from(
@@ -81,12 +93,12 @@ const rox = async () => {
     }
 
     await new Promise((r) => setTimeout(r, 38000));
-
     await page.reload({ waitUntil: "networkidle2" });
+    await new Promise((r) => setTimeout(r, 2000));
 
-    await new Promise((r) => setTimeout(r, 5000));
+    console.log(saldo);
 
-    await page.screenshot({ path: "screen.png" });
+    await page.screenshot({ path: "screen.png", fullPage: true });
   } catch (error) {
     console.error(`Erro interno do servidor: ${error.message}`);
   } finally {
